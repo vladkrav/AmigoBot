18b7aec62a3256e86801fe20aeb0ff34b8b28c01
Vladislav <vladikkravchenko1b@gmail.com>
2021-07-01 18:52:07 +0200

    Some code AMCL

--- AmigoBot_AMCL_sim/web-template/academy _amcl2.py
+++ AmigoBot_AMCL_sim/web-template/academy _amcl2.py
@@ -16,10 +16,11 @@ ROBOT_DIAMETER = 5
 no_particles = 10
 no_random_particles = 5000
 type_localization = True
+show_particles = True
+# Se obtiene el espacio libre del mapa, para despues generar las particulas
 scene = 'scene-1'
 map_name = 'scenes/%s.png' % 'scene-1'
 mapa = mpimg.imread(map_name)[::-1, :, 0]
-show_particles = True
 mark = np.ones((config.ROBOT_DIAMETER, config.ROBOT_DIAMETER))
 convolve_mark = (signal.convolve2d(1-mapa[:, :], mark, mode='same', boundary='fill', fillvalue=0)) / np.sum(mark)
 convolve_mark_overlay = np.copy(mapa)
@@ -28,15 +29,21 @@ convolve_mark_overlay[convolve_mark > threshold]
 map_with_safe_boundary = np.copy(mapa)
 map_with_safe_boundary[convolve_mark > threshold]
 traversable_area = np.stack(np.nonzero(1 - (map_with_safe_boundary.T < 1)), axis=1)
-#Se generan las partículas aleatorias
+
+# Se generan las particulas aleatorias por todo el mapa
 particles_xy_indices = np.random.choice(traversable_area.shape[0], size=no_particles, replace=True)
 particles_xy = traversable_area[particles_xy_indices]
 particles_theta = np.random.uniform(0.0, 2*np.pi, (no_particles, 1)) % (2*np.pi)
 particles = np.hstack([particles_xy, particles_theta])
+
+# Se muestran las particulas generadas
 GUI.showParticles(np.array(particles).tolist())
+
+# Se obtiene la posición inicial del robot 
 aux_pos_x = HAL.getPose3d().x
 aux_pos_y = HAL.getPose3d().y
 aux_pos_yaw = HAL.getPose3d().yaw
+
 noise = True
 distance_differences = []
 angle_differences = []
@@ -46,33 +53,42 @@ env = Environment(scene, 20)
 while True:
     # Enter iterative code!
     distance = math.sqrt((HAL.getPose3d().x - aux_pos_x)**2 + (HAL.getPose3d().y - aux_pos_y)**2)
-    #console.print(distance)
     distance_yaw = abs(aux_pos_yaw - HAL.getPose3d().yaw)
+    # Si se ha recorrido una distancia mayor actualizar o se ha girado mas de 30 grados
     if(distance >= 1 or distance_yaw >= math.pi/6):
-        #Actualización de la posición
+        # Se actualiza la posicion inicial
         aux_pos_x = HAL.getPose3d().x
         aux_pos_y = HAL.getPose3d().y
         aux_pos_yaw = HAL.getPose3d().yaw
+        # Se guarda en un array la posicion
         robot_pos = ((aux_pos_x, aux_pos_y, aux_pos_yaw))
+        
+        # Se vuelve a repetir la generacion de las particulas 
         particles_xy_indices = np.random.choice(traversable_area.shape[0], size=no_particles, replace=True)
         particles_xy = traversable_area[particles_xy_indices]
         particles_theta = np.random.uniform(0.0, 2*np.pi, (no_particles, 1)) % (2*np.pi)
         particles = np.hstack([particles_xy, particles_theta])
         new_particles = np.array(particles).tolist()
         GUI.showParticles(new_particles)
+
+        # Este ha sido el movimiento del robot, se debe aplicar el modelo de movimiento a las particulas
         control = (HAL.getPose3d().x - aux_pos_x, HAL.getPose3d().y - aux_pos_y, HAL.getPose3d().yaw - aux_pos_yaw)
+        
         if(noise == True):
             noise_free_measurements = HAL.getLaserData().values
             noisy_measurements = noise_free_measurements + np.random.normal(0, config.RADAR_NOISE_STD, len(noise_free_measurements))
-            #console.print("Hace correctamente el ruido")
+        
         if(show_particles == True):
+            # Se aplica el modelo de movimiento a las particulas
+            # En primer lugar se generan las matrices de las dimensiones correctas
             new_state, new_v = np.zeros(particles.shape), np.zeros((particles.shape[0], 2))
-            #console.print("Hace correctamente el El show")
             for i in range(particles.shape[0]):
+                # La orientacion de las particulas es la suma de lo girado mas la que tenia
                 robot_thetha = particles[i][2] + control[2]
                 control = np.array(control)
                 theta_control = np.arctan2(control[1], control[0])
                 diff_theta = robot_thetha - theta_control
+
 
                 c, s = np.cos(diff_theta), np.sin(diff_theta)
                 rot = np.array(((c, -s), (s, c)))
@@ -96,12 +112,11 @@ while True:
                 #particle_positions and particle_velocities
                 new_state[i] = new_state_p
                 new_v[i] = v
+
             mm = partial(env.measurement_model, noisy_measurements, noise_free_measurements)
             positions = new_state.tolist()
             p = Pool(100)
-            console.print("Esto es lo que tarda tanto")
             weights = p.map(mm, positions)
-            console.print("Que es lo que tarda tanto?")
             weights = np.array(weights)
             total_weights = np.sum(weights)
             if total_weights == 0:
@@ -109,7 +124,7 @@ while True:
             else:
                 is_weight_valid = True
                 important_weights = weights / total_weights
-            
+            # En este paso se realiza el remuestreo de las particulas
             if(is_weight_valid):
                 particle_resampling_indicies = np.random.choice(new_state.shape[0], new_state.shape[0], replace=True, p=important_weights)
                 particle_resampling = new_state[particle_resampling_indicies]
@@ -119,8 +134,8 @@ while True:
                 particles_theta = np.random.uniform(0.0, 2*np.pi, (no_particles, 1)) % (2*np.pi)
                 particle_resampling = np.hstack([particles_xy, particles_theta])
             
+            # Diferencia de posicion entre la particula y el robot 
             position_differences = particle_resampling[:, :2] - np.array(robot_pos)[:2]
-            #console.print("El problema radica aqui?")
             dists = np.sum(np.abs(position_differences) ** 2, axis=-1) ** (1. / 2)
             distance_differences.append((np.mean(dists), np.std(dists)))
             angle_diffs = (particle_resampling[:, 2] - robot_pos[2]).reshape(-1, 1)
